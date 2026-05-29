4d872a1c9e538b4fbd91ee7f91b793bb80f9d497
Cheese <electronforger@gmail.com>
2026-05-29 13:10:03 -0700

    Initial Commit

---
+++ Word&Page/Word_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_PageApp.swift
+//  Word&Page
+//
+//  Created by Frisbee on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Word_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
